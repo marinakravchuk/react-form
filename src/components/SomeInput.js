d59d5d10b963d95e434600bff50ca24ffc5b6c62
maryna.kravchuk <marina.kravchuk.it@gmail.com>
2023-07-03 09:11:35 +0300

    3

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -1,10 +1,63 @@
 
-import { useState, useRef } from "react"; 
+import { useState} from "react"; 
 
 const SomeInput = (props) => {
-   /* var2 */
+const [enteredName, setEnteredName] = useState("") 
+const [wasNameInputToched, setWasNameInputToched]  = useState(false) 
+const [isFormValid,setIsFormValid] = useState();
+
+const isEnteredNameValid = enteredName.trim() !== ''
+const isNameInputInvalid = !isEnteredNameValid && wasNameInputToched
+
+const nameInputChangeHandler = (event) => {
+  setEnteredName(event.target.value)
+  } 
+const nameinputLostFocusHandler = (event) => {
+  setWasNameInputToched(true)
+}
+
+const formSubmitHandler = (event) => {
+  event.preventDefault();
+  setWasNameInputToched(true)
+
+if (! isEnteredNameValid ){
+return;
+}
+  console.log(enteredName)
+  setEnteredName('')
+  setWasNameInputToched(false)
+
+}
+
+
+const nameInputClasses = isNameInputInvalid ? "form-control  invalid" : "form-control"
+
+  return (
+    <form onSubmit= {formSubmitHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Введите Имя</label>
+        <input type="text" id="name" onChange = {nameInputChangeHandler} 
+        value={enteredName} onBlur={nameinputLostFocusHandler}  />
+        {isNameInputInvalid && <p className="error-text">Введите имя</p>}
+      </div>
+      <div className="form-actions">
+        <button >Отправить</button>
+      </div>
+    </form>
+  );
+};
+
+export default SomeInput;
+
+
+
+
+/* import { useState, useRef } from "react"; 
+
+const SomeInput = (props) => {
+
   const nameInputRef=useRef()
-  /* var1 */
+ 
 const [enteredName, setEnteredName] = useState("") 
 const [isEnteredNameValid, setIsEnteredNameValid] = useState(false)
 const [wasNameInputToched, setWasNameInputToched]  = useState(false) 
@@ -33,9 +86,9 @@ setIsEnteredNameValid(true);
 
   console.log(enteredName)
   console.log(nameInputRef.current.value)
-  setEnteredName('')
+  setEnteredName('') */
 /*   nameInputRef.current.value= "";  not best practice*/
-}
+/* }
 
 const isNameInputInvalid = !isEnteredNameValid && wasNameInputToched
 const nameInputClasses = isNameInputInvalid ? "form-control  invalid" : "form-control"
@@ -56,3 +109,4 @@ const nameInputClasses = isNameInputInvalid ? "form-control  invalid" : "form-co
 };
 
 export default SomeInput;
+ */
